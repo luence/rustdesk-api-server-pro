35e49e3bc631ff885d9149e6d0cf8430c959eafa
Ly545078887 <Ly545078887@users.noreply.github.com>
2026-06-30 23:52:18 +0100

    fix(security): avoid logging sensitive request data in debug logger

--- backend/app/middleware/request_logger.go
+++ backend/app/middleware/request_logger.go
@@ -1,7 +1,6 @@
 package middleware
 
 import (
-	"fmt"
 	"rustdesk-api-server-pro/config"
 
 	"github.com/kataras/iris/v12"
@@ -10,13 +9,7 @@ import (
 func RequestLogger() iris.Handler {
 	return func(context iris.Context) {
 		if config.GetServerConfig().DebugMode && context.Request().RequestURI != "/api/heartbeat" {
-			requestInfo := fmt.Sprintf("▶ %s:%s", context.Method(), context.Request().RequestURI)
-			body, _ := context.GetBody()
-			context.Application().Logger().Info(requestInfo)
-			for header, value := range context.Request().Header {
-				fmt.Println(header+":", value)
-			}
-			fmt.Println(string(body))
+			context.Application().Logger().Infof("▶ %s:%s", context.Method(), context.Request().RequestURI)
 		}
 
 		context.Next()
