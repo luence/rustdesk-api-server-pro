4ed6f29861bc77dd5da1aad84fbcd194c2ac8ded
Ly545078887 <Ly545078887@users.noreply.github.com>
2026-06-30 08:58:08 +0800

    feat: audit address book peer operations

--- backend/app/controller/api/ab_peer.go
+++ backend/app/controller/api/ab_peer.go
@@ -42,20 +42,24 @@ func (c *AddressBookPeerController) HandleAbPeerAdd() mvc.Result {
 
 	var form abform.AbPeer
 	if err := c.readJSONBody(&form); err != nil {
+		c.recordAPIOperationAudit("ab_peer_add", "address_book_peer", abGuid, nil, nil, "failure", err.Error())
 		return c.fail(err)
 	}
 
 	user := c.GetUser()
 	ab, err := c.getAddressBookByGuid(user.Id, abGuid)
 	if err != nil {
+		c.recordAPIOperationAudit("ab_peer_add", "address_book_peer", abGuid, nil, sanitizeAddressBookPeerForAudit(form), "failure", err.Error())
 		return c.fail(err)
 	}
 
 	totalPeers, err := c.addressBookService().CountPeers(user.Id, ab.Id)
 	if err != nil {
+		c.recordAPIOperationAudit("ab_peer_add", "address_book_peer", abGuid, nil, sanitizeAddressBookPeerForAudit(form), "failure", err.Error())
 		return c.fail(err)
 	}
 	if ab.MaxPeer > 0 && totalPeers >= int64(ab.MaxPeer) {
+		c.recordAPIOperationAudit("ab_peer_add", "address_book_peer", abGuid, nil, sanitizeAddressBookPeerForAudit(form), "failure", "exceed_max_devices")
 		return c.failMsg("exceed_max_devices")
 	}
 
@@ -80,8 +84,10 @@ func (c *AddressBookPeerController) HandleAbPeerAdd() mvc.Result {
 		LoginName:         form.LoginName,
 		SameServerPresent: sameServerPresent,
 	}); err != nil {
+		c.recordAPIOperationAudit("ab_peer_add", "address_book_peer", form.Id, nil, sanitizeAddressBookPeerForAudit(form), "failure", err.Error())
 		return c.fail(err)
 	}
+	c.recordAPIOperationAudit("ab_peer_add", "address_book_peer", form.Id, nil, sanitizeAddressBookPeerForAudit(form), "success", "")
 	return c.okText("")
 }
 
@@ -90,12 +96,14 @@ func (c *AddressBookPeerController) HandleAbPeerUpdate() mvc.Result {
 
 	body, err := c.readBodyBytes()
 	if err != nil {
+		c.recordAPIOperationAudit("ab_peer_update", "address_book_peer", abGuid, nil, nil, "failure", err.Error())
 		return c.fail(err)
 	}
 
 	user := c.GetUser()
 	ab, err := c.getAddressBookByGuid(user.Id, abGuid)
 	if err != nil {
+		c.recordAPIOperationAudit("ab_peer_update", "address_book_peer", abGuid, nil, sanitizeAddressBookPeerUpdateBodyForAudit(body), "failure", err.Error())
 		return c.fail(err)
 	}
 
@@ -105,11 +113,14 @@ func (c *AddressBookPeerController) HandleAbPeerUpdate() mvc.Result {
 
 	has, err := c.addressBookService().UpdatePeer(cmd)
 	if err != nil {
+		c.recordAPIOperationAudit("ab_peer_update", "address_book_peer", cmd.RustdeskID, nil, sanitizeAddressBookPeerUpdateBodyForAudit(body), "failure", err.Error())
 		return c.fail(err)
 	}
 	if !has {
+		c.recordAPIOperationAudit("ab_peer_update", "address_book_peer", cmd.RustdeskID, nil, sanitizeAddressBookPeerUpdateBodyForAudit(body), "failure", "peer not found")
 		return c.failMsg("peer not found")
 	}
+	c.recordAPIOperationAudit("ab_peer_update", "address_book_peer", cmd.RustdeskID, nil, sanitizeAddressBookPeerUpdateBodyForAudit(body), "success", "")
 	return c.okText("")
 }
 
@@ -118,12 +129,18 @@ func (c *AddressBookPeerController) HandleAbPeerDelete() mvc.Result {
 
 	var ids []string
 	if err := c.Ctx.ReadBody(&ids); err != nil {
+		c.recordAPIOperationAudit("ab_peer_delete", "address_book_peer", abGuid, nil, nil, "failure", err.Error())
 		return c.fail(err)
+	}
+	if len(ids) == 0 {
+		c.recordAPIOperationAudit("ab_peer_delete", "address_book_peer", abGuid, map[string]any{"ids": ids}, nil, "failure", "NoPeerIds")
+		return c.failMsg("NoPeerIds")
 	}
 
 	user := c.GetUser()
 	ab, err := c.getAddressBookByGuid(user.Id, abGuid)
 	if err != nil {
+		c.recordAPIOperationAudit("ab_peer_delete", "address_book_peer", abGuid, map[string]any{"ids": ids}, nil, "failure", err.Error())
 		return c.fail(err)
 	}
 
@@ -132,9 +149,11 @@ func (c *AddressBookPeerController) HandleAbPeerDelete() mvc.Result {
 		AbID:   ab.Id,
 		IDs:    ids,
 	}); err != nil {
+		c.recordAPIOperationAudit("ab_peer_delete", "address_book_peer", abGuid, map[string]any{"ids": ids}, nil, "failure", err.Error())
 		return c.fail(err)
 	}
 
+	c.recordAPIOperationAudit("ab_peer_delete", "address_book_peer", abGuid, map[string]any{"ids": ids}, nil, "success", "")
 	return c.ok()
 }
 
@@ -180,4 +199,38 @@ func parseAddressBookPeerUpdate(body []byte) core.AddressBookPeerUpdateCommand {
 	}
 
 	return cmd
+}
+
+func sanitizeAddressBookPeerForAudit(form abform.AbPeer) map[string]any {
+	return map[string]any{
+		"id":                 form.Id,
+		"hash":               form.Hash,
+		"username":           form.Username,
+		"password_changed":   form.Password != "",
+		"hostname":           form.Hostname,
+		"platform":           form.Platform,
+		"alias":              form.Alias,
+		"tags":               form.Tags,
+		"note":               form.Note,
+		"force_always_relay": form.ForceAlwaysRelay,
+		"rdp_port":           form.RdpPort,
+		"rdp_username":       form.RdpUsername,
+		"login_name":         form.LoginName,
+		"same_server":        form.SameServer != "",
+	}
+}
+
+func sanitizeAddressBookPeerUpdateBodyForAudit(body []byte) map[string]any {
+	result := map[string]any{
+		"id": gjson.GetBytes(body, "id").String(),
+	}
+	for _, key := range []string{"tags", "alias", "hash", "note", "username", "hostname", "platform"} {
+		if v := gjson.GetBytes(body, key); v.Exists() {
+			result[key] = v.Value()
+		}
+	}
+	if v := gjson.GetBytes(body, "password"); v.Exists() {
+		result["password_changed"] = v.String() != ""
+	}
+	return result
 }
